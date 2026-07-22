382733c21bb08ba3ef9e0117e28ff72a8cdcebc7
mgn12 <marcosnuero11@gmail.com>
2026-07-22 18:46:21 +0200

    encripted password added at entity user

--- backend/src/resources/users/entities/user.entity.ts
+++ backend/src/resources/users/entities/user.entity.ts
@@ -1,4 +1,6 @@
 import {
+  BeforeInsert,
+  BeforeUpdate,
   Column,
   CreateDateColumn,
   Entity,
@@ -7,6 +9,7 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Profile } from '../../profiles/entities/profiles.entity';
+import * as bcrypt from 'bcrypt';
 
 @Entity('users')
 export class User {
@@ -34,4 +37,13 @@ export class User {
   @OneToOne(() => Profile, { cascade: true, onDelete: 'CASCADE' })
   @JoinColumn()
   profile: Profile;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    if (this.password && !this.password.startsWith('$2b$')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  }
 }
